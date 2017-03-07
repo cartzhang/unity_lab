09c4090aacaab12eaa854591ea8068b5433b91b3
cartzhang <cart033@sina.com>
2017-03-07 18:35:24 +0800

    unity asset export to .obj

---
+++ ExportFbx/UnityAssetExportFBX/Assets/Test/CreatAsset.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEditor;
+
+public class CreatAsset : MonoBehaviour
+{
+
+    [MenuItem("GameObject/Create Material")]
+    static void CreateMaterial()
+    {
+        // Create a simple material asset
+
+        Material material = new Material(Shader.Find("Specular"));        
+        AssetDatabase.CreateAsset(material, "Assets/MyMaterial.mat");
+
+        // Print the path of the created asset
+        Debug.Log(AssetDatabase.GetAssetPath(material));
+    }
+}
